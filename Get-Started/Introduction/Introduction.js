4490c9e743db7ed96094e27bdb0332145eeff67d
LEMONxSLICES <aliwascreatingbaby@gmail.com>
2023-12-26 10:03:54 +0200

    Updated

---
+++ Get-Started/Introduction/Introduction.js
@@ -0,0 +1,16 @@
+let Theme = "light" ;
+
+function SwitchTheme()
+{
+    if(Theme == "light") 
+    {
+        document.body.style.backgroundColor = "#1d1d1dff" ;
+        document.body.style.color = "#ecececff" ;
+        Theme = "dark" ;
+    }
+    else
+    {
+        document.body.style.backgroundColor = "#ecececff" ;
+        document.body.style.color = "#1d1d1dff" ;
+    }
+}
